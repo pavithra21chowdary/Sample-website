73c4d95def09d08a030640c1bcda7a608fb17fa8
pavithra21chowdary <83344517+pavithra21chowdary@users.noreply.github.com>
2022-01-25 13:02:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function home(){
+    var url="index.html"
+    alert ('HI'+ url);
+}
+function about(){
+    var url1="https://www.geeksforgeeks.org/javascript-tutorial/"
+    alert(url1);
+}
+function getUrl(){
+
+}
